f4fd3788977613ed4cbe8b1d8839bfb94828129e
rocco <rocco.mormont@gmail.com>
2018-08-28 00:05:53 +0800

    :bug: modify reference

--- src/mobile/views/Home.vue
+++ src/mobile/views/Home.vue
@@ -8,7 +8,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/pc/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/mobile/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
   components: {
